1c7cc11e7b9ab2781bf2377ca40f499b1cd255e5
blackgold0112 <blackgold20030112@gmail.com>
2019-04-27 20:55:52 -0400

    electron-summercash: Removed react sources.

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('../build/index.html')
+  mainWindow.loadFile('./web/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
