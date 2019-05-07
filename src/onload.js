9f726e83f4a2b797be50fd4980f5cde6f66fc986
blackgold0112 <blackgold20030112@gmail.com>
2019-05-07 08:01:36 -0400

    src: Added cookie sync.

--- src/onload.js
+++ src/onload.js
@@ -1,3 +1,6 @@
+// Get session
+const { session } = require("electron");
+
 // Get win electron title-bar
 const ElectronTitlebarWindows = require("electron-titlebar-windows");
 
@@ -6,6 +9,8 @@ function init() {
   const titlebar = new ElectronTitlebarWindows(); // Initialize title bar
 
   titlebar.appendTo(window.document.getElementsByClassName("drag")); // Append to drag DOM element
+
+  window.cookies = session.defaultSession.cookies; // Set cookies
 }
 
 init(); // Init
