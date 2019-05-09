98f2f0c66079edb257c27c193e1f440122e03a07
blackgold0112 <blackgold20030112@gmail.com>
2019-05-09 18:51:16 -0400

    src: Fixed windows title bar.

--- src/preload.js
+++ src/preload.js
@@ -1,6 +1,9 @@
 // Get IPC renderer
 const { ipcRenderer, clipboard } = require("electron");
 
+// Import custom titlebar
+const customTitlebar = require("custom-electron-titlebar");
+
 // init
 function init() {
   // add global variables to web page
@@ -10,6 +13,10 @@ function init() {
   if (process.platform === "win32") {
     // Check is windows
     window.isWindows = true; // Set is windows
+
+    new customTitlebar.Titlebar({
+      backgroundColor: customTitlebar.Color.fromHex("#2162ce")
+    }); // Initialize custom titlebar
   }
 
   window.navigator.clipboard = clipboard; // Set window clipboard
